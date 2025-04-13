f0463e3fa0ccc979d54223772915593a65a8c329
abhayra12 <abhayahirkar2@gmail.com>
2025-04-13 18:23:09 +0000

    updated tf config

--- terraform/main.tf
+++ terraform/main.tf
@@ -6,31 +6,54 @@ terraform {
     }
   }
 }
+
 provider "google" {
   credentials = "../gcp-creds.json"
-  project     = "eastern-amp-449614-e1"
-
+  project     = var.project_id
+  region      = var.location
 }
 
 resource "google_storage_bucket" "gcp-storage" {
   name          = var.gcs_bucket_name
-  location = var.location
+  location      = var.location
   force_destroy = true
-
+  
+  uniform_bucket_level_access = true
+  
   lifecycle_rule {
     condition {
       age = 3
     }
-
+    
     action {
       type = "Delete"
     }
   }
 }
 
-resource "google_bigquery_dataset" "bigquery-dataset" {
-  dataset_id = var.bq_dataset_name
-  location = var.location
-  project = "eastern-amp-449614-e1"
-  delete_contents_on_destroy = true
+# Create "code" folder in the bucket
+resource "google_storage_bucket_object" "code_folder" {
+  name    = "code/"
+  content = " "  # Empty content for folder creation
+  bucket  = google_storage_bucket.gcp-storage.name
 }
+
+
+resource "google_bigquery_dataset" "bigquery-dataset" {
+  dataset_id                  = var.bq_dataset_name
+  location                    = var.location
+  project                     = var.project_id
+  delete_contents_on_destroy  = true
+  
+  default_table_expiration_ms = 3600000 * 24 * 3 # 3 days
+}
+
+# Set appropriate IAM permissions for the bucket
+resource "google_storage_bucket_iam_binding" "bucket_admin" {
+  bucket = google_storage_bucket.gcp-storage.name
+  role = "roles/storage.admin"
+  members = [
+    "serviceAccount:terraform-runner@${var.project_id}.iam.gserviceaccount.com",
+  ]
+}
+
--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,16 +1,19 @@
 variable "location" {
-  description = "My location"
-  default = "asia-south1"
+  description = "GCP region for resources"
+  default     = "asia-south1"
 }
 
-variable "bq_dataset_name"{
-    description = "Bigquery Dataset name"
-    default = "lending_db"  # Change this
+variable "project_id" {
+  description = "GCP Project ID"
+  default     = "eastern-amp-449614-e1"
 }
 
-
+variable "bq_dataset_name" {
+  description = "Bigquery Dataset name"
+  default     = "lending_db"
+}
 
 variable "gcs_bucket_name" {
   description = "GCS storage bucket name"
-  default = "lending_ara"  # Change this (use only lowercase and hyphens)
+  default     = "lending_ara"
 }
